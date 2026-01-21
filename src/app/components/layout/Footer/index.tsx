34641671c5d3fce6331c84f0233c07177caf3bb1
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-21 20:05:12 -0300

    feat: create Footer component and its CSS

---
+++ src/app/components/layout/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.scss';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h2>STARSOFT © TODOS OS DIREITOS RESERVADOS</h2>
+    </footer>
+  );
+}
