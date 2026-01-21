1513c50e54db60c46bcf608bc241edf03fcd0170
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-20 23:42:42 -0300

    feat: add home page and its css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,159 @@
+import Image from 'next/image';
+import logo from '../../public/logo.png';
+import cart from '../../public/Bag.png';
+import nft_1 from '../../public/nft_1.png';
+import icon_price from '../../public/icon_price.png';
+
+import styles from './page.module.scss';
+
 export default function Home() {
   return (
-    <div>
-      <h1>marketplace de NFTs</h1>
-    </div>
+    <>
+      <div className={styles.header}>
+        <header>
+          <Image src={logo} alt="Logo" />
+          <div className={styles.cart}>
+            <Image className={styles.cartIcon} src={cart} alt="Cart" />
+            <span>0</span>
+          </div>
+        </header>
+      </div>
+
+      <section className={styles.containerNFT}>
+        <div className={styles.cardNFT}>
+          <div className={styles.containerImageNFT}>
+            <Image className="" src={nft_1} alt="Logo" />
+          </div>
+
+          <div className={styles.containerContent}>
+            <h2>Lorem inpsum</h2>
+            <p>Redesigned from scratch and completely revised.</p>
+            <div className={styles.content}>
+              <Image src={icon_price} alt="Cart" />
+              <span>32 ETH</span>
+            </div>
+            <button>Comprar</button>
+          </div>
+        </div>
+
+        <div className={styles.cardNFT}>
+          <div className={styles.containerImageNFT}>
+            <Image className="" src={nft_1} alt="Logo" />
+          </div>
+
+          <div className={styles.containerContent}>
+            <h2>Lorem inpsum</h2>
+            <p>Redesigned from scratch and completely revised.</p>
+            <div className={styles.content}>
+              <Image src={icon_price} alt="Cart" />
+              <span>32 ETH</span>
+            </div>
+            <button>Comprar</button>
+          </div>
+        </div>
+
+        <div className={styles.cardNFT}>
+          <div className={styles.containerImageNFT}>
+            <Image className="" src={nft_1} alt="Logo" />
+          </div>
+
+          <div className={styles.containerContent}>
+            <h2>Lorem inpsum</h2>
+            <p>Redesigned from scratch and completely revised.</p>
+            <div className={styles.content}>
+              <Image src={icon_price} alt="Cart" />
+              <span>32 ETH</span>
+            </div>
+            <button>Comprar</button>
+          </div>
+        </div>
+
+        <div className={styles.cardNFT}>
+          <div className={styles.containerImageNFT}>
+            <Image className="" src={nft_1} alt="Logo" />
+          </div>
+
+          <div className={styles.containerContent}>
+            <h2>Lorem inpsum</h2>
+            <p>Redesigned from scratch and completely revised.</p>
+            <div className={styles.content}>
+              <Image src={icon_price} alt="Cart" />
+              <span>32 ETH</span>
+            </div>
+            <button>Comprar</button>
+          </div>
+        </div>
+
+        <div className={styles.cardNFT}>
+          <div className={styles.containerImageNFT}>
+            <Image className="" src={nft_1} alt="Logo" />
+          </div>
+
+          <div className={styles.containerContent}>
+            <h2>Lorem inpsum</h2>
+            <p>Redesigned from scratch and completely revised.</p>
+            <div className={styles.content}>
+              <Image src={icon_price} alt="Cart" />
+              <span>32 ETH</span>
+            </div>
+            <button>Comprar</button>
+          </div>
+        </div>
+
+        <div className={styles.cardNFT}>
+          <div className={styles.containerImageNFT}>
+            <Image className="" src={nft_1} alt="Logo" />
+          </div>
+
+          <div className={styles.containerContent}>
+            <h2>Lorem inpsum</h2>
+            <p>Redesigned from scratch and completely revised.</p>
+            <div className={styles.content}>
+              <Image src={icon_price} alt="Cart" />
+              <span>32 ETH</span>
+            </div>
+            <button>Comprar</button>
+          </div>
+        </div>
+
+        <div className={styles.cardNFT}>
+          <div className={styles.containerImageNFT}>
+            <Image className="" src={nft_1} alt="Logo" />
+          </div>
+
+          <div className={styles.containerContent}>
+            <h2>Lorem inpsum</h2>
+            <p>Redesigned from scratch and completely revised.</p>
+            <div className={styles.content}>
+              <Image src={icon_price} alt="Cart" />
+              <span>32 ETH</span>
+            </div>
+            <button>Comprar</button>
+          </div>
+        </div>
+
+        <div className={styles.cardNFT}>
+          <div className={styles.containerImageNFT}>
+            <Image className="" src={nft_1} alt="Logo" />
+          </div>
+
+          <div className={styles.containerContent}>
+            <h2>Lorem inpsum</h2>
+            <p>Redesigned from scratch and completely revised.</p>
+            <div className={styles.content}>
+              <Image src={icon_price} alt="Cart" />
+              <span>32 ETH</span>
+            </div>
+            <button>Comprar</button>
+          </div>
+        </div>
+      </section>
+
+      <button className={styles.loadMore}>Carregar mais</button>
+
+      <footer className={styles.footer}>
+        <h2>STARSOFT © TODOS OS DIREITOS RESERVADOS</h2>
+      </footer>
+    </>
   );
 }
