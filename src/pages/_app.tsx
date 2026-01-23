4fb9096769f15abbd031e14d0f00780e327b5152
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-22 22:05:39 -0300

    feat: migrate to Next.js page router and add app file with Query Client config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '@/styles/globals.scss';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60 * 1000,
+            gcTime: 5 * 60 * 1000,
+            retry: 1,
+            refetchOnWindowFocus: false
+        },
+    },
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    );
+}
