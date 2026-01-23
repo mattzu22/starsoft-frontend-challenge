88989b37e6260fa35f80d638f740e3a1b89ef549
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-22 22:21:41 -0300

    chore: move files from app folder to src

---
+++ src/components/layout/Header/index.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import styles from './Header.module.scss';
+import logo from '@/public/logo.png';
+import cart from '@/public/Bag.png';
+
+interface HeaderProps {
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function Header({ setIsOpen}: HeaderProps) {
+  function openModal() {
+     setIsOpen(true)
+  }
+
+  return (
+    <div className={styles.header}>
+      <header>
+        <Image src={logo} alt="Logo" />
+        <div className={styles.cart}>
+          <button onClick={openModal}>
+            <Image className={styles.cartIcon} src={cart} alt="Cart" />
+          </button>
+          <span>0</span>
+        </div>
+      </header>
+    </div>
+  );
+}
