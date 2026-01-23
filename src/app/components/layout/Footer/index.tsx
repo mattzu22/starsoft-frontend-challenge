a4cc0d8ff1fb9304917c850755458ddbe12dc64e
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-22 21:41:42 -0300

    refactor: move app folder contents to src

--- src/app/components/layout/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.scss';
-
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <h2>STARSOFT © TODOS OS DIREITOS RESERVADOS</h2>
-    </footer>
-  );
-}
