3806e68afbcde049c3d6808a3741fabff21b75e7
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-22 22:07:18 -0300

    feat: migrate to Next.js page router and add Document file for base HTML structure

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+
+import '@/styles/globals.scss';
+import { Html, Head, Main, NextScript } from "next/document";
+
+
+export default function Document() {
+  return (
+    <Html lang="pt-br">
+      <Head >
+        <meta charSet="utf-8" />
+
+        <meta name="description" content="Marketplace de NFTs" />
+        <meta name="keywords" content="NFT, marketplace, blockchain, arte digital, colecionáveis" />
+        <link rel="icon" href="/favicon.ico" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
