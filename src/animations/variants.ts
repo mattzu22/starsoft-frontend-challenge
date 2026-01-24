1a5f7560e8215a47524f4452df3d643fbdc1195a
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-24 18:12:23 -0300

    feat: add animation to cart icon and item quantity

--- src/animations/variants.ts
+++ src/animations/variants.ts
@@ -41,3 +41,21 @@ export const quantityVariants: Variants = {
   exit: { opacity: 0, scale: 0.5, y: 5 } 
 };
 
+export const cartIconVariants: Variants = {
+  initial: { rotate: 0 },
+  rotated: { rotate: -100, transition: { duration: 0.4, ease: "easeOut" } },
+};
+
+export const totalItemsVariants: Variants = {
+  initial: { opacity: 1, scale: 1, x: 0, y: 0 },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    x: [0, -2, 2, -1, 1, 0],
+    y: 0,
+    transition: {
+      x: { duration: 0.3, ease: "easeInOut" },
+    }
+  },
+  exit: { opacity: 0, scale: 0.8, y: 5, transition: { duration: 0.2, ease: "easeIn" } }
+};
--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -3,14 +3,20 @@ import styles from './Header.module.scss';
 import logo from '@/public/logo.png';
 import cart from '@/public/Bag.png';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectTotalItems } from '@/src/store/cart/selectors';
+import { selectTotalItems, selectIsCartOpen } from '@/src/store/cart/selectors';
 import { toggleCart } from '@/src/store/cart/cartSlice';
+import { AnimatePresence, motion } from 'framer-motion';
+
+import { cartIconVariants, totalItemsVariants } from "@/src/animations/variants"
+
+
 
 export default function Header() {
   const dispatch = useDispatch();
+  const isCartOpen = useSelector(selectIsCartOpen);
 
   function handleToggleCart() {
-     dispatch(toggleCart());
+    dispatch(toggleCart());
   }
 
   const totalItems = useSelector(selectTotalItems)
@@ -20,10 +26,30 @@ export default function Header() {
       <header>
         <Image src={logo} alt="Logo" />
         <div className={styles.cart}>
-          <button onClick={handleToggleCart}>
-            <Image className={styles.cartIcon} src={cart} alt="Cart" />
-          </button>
-          <span>{totalItems}</span>
+          <motion.button
+            onClick={handleToggleCart}
+          >
+            <motion.img
+              className={styles.cartIcon}
+              src={cart.src}
+              alt="Cart"
+              variants={cartIconVariants}
+              initial="initial"
+              animate={isCartOpen ? "rotated" : "initial"}
+            />
+          </motion.button>
+          <AnimatePresence mode="wait" initial={false}>
+            <motion.span
+              key={totalItems}
+              variants={totalItemsVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              className={styles.totalItemsCount}
+            >
+              {totalItems}
+            </motion.span>
+          </AnimatePresence>
         </div>
       </header>
     </div>
