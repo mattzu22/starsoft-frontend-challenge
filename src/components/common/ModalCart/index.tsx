e7139487fb104f3ef6c35e298f042f06c6e69f3a
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-23 21:14:44 -0300

    refactor: add usage of components and useSelector to fetch cart items

--- src/components/common/ModalCart/index.tsx
+++ src/components/common/ModalCart/index.tsx
@@ -1,82 +1,27 @@
 import Image from 'next/image';
-import arrow from '@/public/Arrow - Left.png';
 import iconDelete from '@/public/Delete.png';
 import spiritLanter from '@/public/spirit-lantern 1.png';
 
 import styles from './ModalCart.module.scss';
 import Button from '../Button';
 
+import { HeaderModal } from './Header';
+import { useSelector } from 'react-redux';
+import { selectCartItems } from '@/src/store/cart/selectors';
+import CardNFTModal from './CardNFTModal';
+
 export default function ModalCart() {
+  const cartItems = useSelector(selectCartItems);
+
   return (
     <div className={styles.containerModal}>
-      <header className={styles.header}>
-        <button>
-          <Image src={arrow} alt="Cart" width={33} height={33} />
-        </button>
-
-        <h2>Mochila de Compras</h2>
-      </header>
+      <HeaderModal />
 
       <div className={styles.containerCardNFT}>
-        <div className={styles.cardNFT}>
-          <div className={styles.imageNFT}>
-            <Image
-              src={spiritLanter}
-              alt="spiritLanter"
-              width={139}
-              height={139}
-            />
-          </div>
-          <div className={styles.content}>
-            <h2>ITEM 2</h2>
-            <p>Redesigned from scratch and completely revised</p>
-            <div className={styles.price}>
-              <Image src="/icon_price.png" alt="Cart" width={29} height={29} />
-              <span>12 ETH</span>
-            </div>
-            <div className={styles.containerQuantity}>
-              <div className={styles.quantity}>
-                <button className={styles.subtract}>-</button>
-                <span>1</span>
-                <button className={styles.add}>+</button>
-              </div>
-
-              <button className={styles.buttonDelete}>
-                <Image src={iconDelete} alt="Cart" width={25} height={25} />
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.cardNFT}>
-          <div className={styles.imageNFT}>
-            <Image
-              src={spiritLanter}
-              alt="spiritLanter"
-              width={139}
-              height={139}
-            />
-          </div>
-          <div className={styles.content}>
-            <h2>ITEM 2</h2>
-            <p>Redesigned from scratch and completely revised</p>
-            <div className={styles.price}>
-              <Image src="/icon_price.png" alt="Cart" width={29} height={29} />
-              <span>12 ETH</span>
-            </div>
-            <div className={styles.containerQuantity}>
-              <div className={styles.quantity}>
-                <button className={styles.subtract}>-</button>
-                <span>1</span>
-                <button className={styles.add}>+</button>
-              </div>
-
-              <button className={styles.buttonDelete}>
-                <Image src={iconDelete} alt="Cart" width={25} height={25} />
-              </button>
-            </div>
-          </div>
-        </div>
+         {cartItems.length === 0 && <h2>Carrinho vazio</h2>}
+        {cartItems.map(item => (
+          <CardNFTModal key={item.id} item={item} />
+        ))}
       </div>
 
       <div className={styles.total}>
