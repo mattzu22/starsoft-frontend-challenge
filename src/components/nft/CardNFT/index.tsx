c30aeb62f01e7fa782a683a92e4a0ddd75eace25
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-23 21:17:29 -0300

    feat: implement logic to add items to cart

--- src/components/nft/CardNFT/index.tsx
+++ src/components/nft/CardNFT/index.tsx
@@ -3,8 +3,21 @@ import styles from './CardNFT.module.scss';
 
 import Button from '@/components/common/Button';
 import { NFTprops } from '@/hooks/useNFTs';
+import { useDispatch } from 'react-redux';
+import { addItem } from '@/src/store/cart/cartSlice';
 
 export default function CardNFT({ data }: { data: NFTprops }) {
+  const dispatch = useDispatch();
+
+    function handleAddToCart() {
+    dispatch(addItem({
+      id: data.id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      quantity: 1
+    }))
+  }
   return (
     <div className={styles.cardNFT}>
       <Image className={styles.imageNFT} src={data.image} alt={data.name} width={216} height={195} />
@@ -16,7 +29,7 @@ export default function CardNFT({ data }: { data: NFTprops }) {
           <Image src="/icon_price.png" alt="Cart" width={29} height={29} />
           <span>{data.price} ETH</span>
         </div>
-        <Button animation="drop" textHover="Adicionar ao carrinho">
+        <Button animation="drop" textHover="Adicionar ao carrinho" onClick={handleAddToCart}>
           Comprar
         </Button>
       </div>
