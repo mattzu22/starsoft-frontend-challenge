bf8b6971bb1716d8c72706ec5f6e710f5f0469d0
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-25 19:02:30 -0300

    feat:  install and configure the jest library

---
+++ jest.config.mjs
@@ -0,0 +1,30 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+    '!src/**/__tests__/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+};
+
+export default createJestConfig(customJestConfig);
--- src/store/cart/selectors.ts
+++ src/store/cart/selectors.ts
@@ -1,6 +1,6 @@
 import { RootState }  from "@/store/index";
 
-export const selectNFTpropss = (state: RootState) => state.cart.cart;
+export const selectNFTItems = (state: RootState) => state.cart.cart;
 export const selectTotalItems = (state: RootState) => state.cart.cart.length || 0;
 export const selectTotalPrice = (state: RootState) => state.cart.cart.reduce((total, item) => total + item.price * item.quantity, 0) || 0;
 export const selectIsCartOpen = (state: RootState) => state.cart.isCartOpen;
