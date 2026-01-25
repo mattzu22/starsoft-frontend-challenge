1331bd18d932e165f1253c77d08daf20ac73fbda
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-25 10:54:17 -0300

    feat: implement redux-persist for cart state

--- src/store/StoreProvider.tsx
+++ src/store/StoreProvider.tsx
@@ -1,10 +1,13 @@
 import { Provider } from "react-redux";
-import { store } from ".";
+import { persistorCart, store } from ".";
+import { PersistGate } from "redux-persist/integration/react";
 
-export default function StoreProvider({children}: {children: React.ReactNode}) {
+export default function StoreProvider({ children }: { children: React.ReactNode }) {
     return (
         <Provider store={store}>
-            {children}
+            <PersistGate loading={null} persistor={persistorCart} >
+                {children}
+            </PersistGate>
         </Provider>
     )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cart/cartSlice";
 
-export const store= configureStore({
+import storage from "redux-persist/lib/storage";
+import {
+    persistStore, persistReducer, FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+    key: "cart",
+    storage
+}
+
+const persistedCartReducer = persistReducer(persistConfig, cartSlice.reducer)
+
+export const store = configureStore({
     reducer: {
-        cart: cartSlice.reducer
-    }
+        cart: persistedCartReducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
 
+
+export const persistorCart = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
