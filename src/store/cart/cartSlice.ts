25da603e6ff3663f8469477043a31545b5044bd9
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-25 13:42:28 -0300

    fix: correct interface import

--- src/store/cart/cartSlice.ts
+++ src/store/cart/cartSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { NFTprops } from "@/src/types/storeCart";
+import { NFTprops } from "@/src/types/nft";
 
 
 const cartSlice = createSlice({
