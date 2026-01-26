3772c51640f1a1dfdea80e1605a8cc5c69ffe3a1
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-26 16:50:40 -0300

    refcator: remove unused AnimatePresence component

--- src/components/home-nft/ListCardsNFT/index.tsx
+++ src/components/home-nft/ListCardsNFT/index.tsx
@@ -1,21 +1,18 @@
 import { NFTprops } from "@/types/nft";
 import CardNFT from "../CardNFT";
 import styles from "./ListCardsNFT.module.scss"
-import { AnimatePresence, motion } from "framer-motion";
 
 export default function ListCardsNFT({ data, isError }: { data: NFTprops[], isError: boolean }) {
     return (
-        <motion.div className={styles.containerNFT} layout>
-            <AnimatePresence mode="popLayout">
-                {
-                    isError ?
-                        <p>Erro ao carregar os NFTs</p>
-                        :
-                        data?.map(nft => (
-                            <CardNFT key={nft.id} data={nft} />
-                        ))
-                }
-            </AnimatePresence>
-        </motion.div>
+        <div className={styles.containerNFT}>
+            {
+                isError ?
+                    <p>Erro ao carregar os NFTs</p>
+                    :
+                    data?.map(nft => (
+                        <CardNFT key={nft.id} data={nft} />
+                    ))
+            }
+        </div>
     )
 }
