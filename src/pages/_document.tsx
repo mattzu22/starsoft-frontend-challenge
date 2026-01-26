450d98cbb838a18be8295eee72ac9256d0eb7ff9
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2026-01-26 16:53:59 -0300

    feat: add svg to project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,4 @@
-
-import '@/styles/globals.scss';
 import { Html, Head, Main, NextScript } from "next/document";
-
 
 export default function Document() {
   return (
@@ -11,7 +8,7 @@ export default function Document() {
 
         <meta name="description" content="Marketplace de NFTs" />
         <meta name="keywords" content="NFT, marketplace, blockchain, arte digital, colecionáveis" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
